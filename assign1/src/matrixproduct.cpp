9a17e181a0058d21c3082e6e11bc5e88c654c283
Tomás Rodrigues <tomasvrrodrigues@gmail.com>
2025-02-25 09:56:32 +0000

    MultOnLine function push for testing

--- assign1/src/matrixproduct.cpp
+++ assign1/src/matrixproduct.cpp
@@ -75,8 +75,70 @@ void OnMult(int m_ar, int m_br)
 // add code here for line x line matriz multiplication
 void OnMultLine(int m_ar, int m_br)
 {
-    
-    
+    SYSTEMTIME Time1, Time2; // variables to store the time
+
+	char st[100]; // string to store the time
+	int i, j, k; // loop variables
+
+	double *pha, *phb, *phc; // pointers to the matrices
+	
+
+
+	//allocating memory for the matrices (later to be "freed")
+	pha = (double *)malloc((m_ar * m_ar) * sizeof(double)); 
+	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+
+	// Initialize the matrices (a and b respectively)
+	// pha is a matrix of 1's
+	// phb is a matrix of 1,2,3,4,5,6,7,8,9,10
+    for (i=0; i<m_ar; i++){
+		for (int j=0; j<m_ar; j++){
+			pha[i*m_ar + j] = (double)1.0;
+		}
+	}
+
+	for (i = 0; i < m_br; i++)
+	{
+        for (j = 0; j < m_br; j++){
+            phb[i * m_br + j] = i + 1;
+		}
+	}
+
+
+	Time1 = clock(); //Initiate the clock, starting the performance measure
+
+	for(i=0; i<m_ar; i++){
+		for (k=0; i<m_ar; j++){
+			double valA = pha[i * m_ar + k]; //Value of the element at index (i, k) in the matrix A
+			for (j=0; k<m_br; k++){		//Multiply by the entire row of the matrix B
+				phc[i*m_ar+j] += valA * phb[k*m_br+j]; //Multiplying the value of the element at index (i, k) in the matrix A by the value of the element at index (k, j) in the matrix B and adding it to the element at index (i, j) in the matrix C
+			}
+		}
+	}
+
+	Time2 = clock(); //Stop the clock, ending the performance measure
+
+
+	//Time to perform the operation calculation and display
+	sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+	cout << st; 
+
+
+	// display 10 elements of the result matrix tto verify correctness
+	cout << "Result matrix: " << endl;
+	for(i=0; i<1; i++)
+	{	for(j=0; j<min(10,m_br); j++)
+			cout << phc[j] << " ";
+	}
+	cout << endl;
+
+    free(pha);
+    free(phb);
+    free(phc);
+
+
 }
 
 // add code here for block x block matriz multiplication
